81d9ba47632b6f771aec2fc91310b22516a4a26e
Thibaud Chassin <thibaud.chassin@heig-vd.ch>
2021-01-24 22:06:43 +0100

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: 'portal-front',
+    script: 'serve -s dist -l 443',
+
+    // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    }
+  }],
+};
